cbb3c56b0089766acd2fd94771946334f4e4c7be
unknown <>
2022-11-30 14:08:46 +0300

    0.0

---
+++ components/AppFooter.vue
@@ -0,0 +1,10 @@
+<template>
+    <v-footer absolute app>
+
+        <v-container class="px-4" style="max-width:1400px">
+            <span>&copy; {{ new Date().getFullYear() }}</span>
+        </v-container>
+
+    </v-footer>
+</template>
+
