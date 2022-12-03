32f2ed65a297c8b022201dd2fef2c1df35afaf62
unknown <>
2022-12-03 12:35:17 +0300

    rates list done

--- components/RatesList.vue
+++ components/RatesList.vue
@@ -1,14 +1,13 @@
 <template>
-    <v-card v-if="dataa" class="px-4 rounded-xl mb-4 py-4" style="max-width: 300px">
-        <v-card-title primary-title class="px-0 py-2 justify-center">
-            1 USD =
-        </v-card-title>
+    <v-card v-if="dataJson" class="px-4 rounded-xl mb-4 py-4" style="max-width: 300px">
         <v-list outlined class="list my-0 mx-auto" style="width:250px">
-            <v-list-item v-for="(currency, index) in dataa.data" :key="index" class="list__item">
+            <v-list-item v-for="(currency, index) in dataJson.data" :key="index" class="list__item">
                 <v-list-item-content>
-                    <v-list-item-title class="d-flex justify-space-between" style="font-size: 28px;">
-                        <span class="primary--text"> {{ currency.code }}:</span>
-                        <span>{{ (1 / currency.value).toFixed(4) }}</span>
+                    <v-list-item-title class="d-flex justify-space-between" style="font-size: 24px;">
+                        <span class="primary--text">1 {{ currency.code }}=</span>
+                        <span class="d-flex align-center ml-4" style="font-size: 21px; overflow-x: auto">
+                            {{ getValidNumber(currency.value) }}
+                        </span>
                     </v-list-item-title>
                 </v-list-item-content>
             </v-list-item>
@@ -29,34 +28,52 @@ export default {
             required: true
         },
     },
-    mounted() {
-        this.name();
-    },
     methods: {
-        name() {
-            this.dataa = require('@@/static/sample.json');
+        getValidNumber(val) {
+            let fix = 4;
+            let result = (1 / val).toFixed(fix)
+
+            // 1: 50.0000 -> 50
+            // 2: 0.0000 -> 0.0000002
+            if (result[0] !== '0') {
+                while ((result[result.length - 1] === '0') && (fix > 0)) {
+                    fix--;
+                    result = Number(result).toFixed(fix);
+                }
+            }
+            else if ((result[result.length - 1] === '0')) {
+                while ((result[result.length - 1] === '0') && (fix < 100)) {
+                    fix++;
+                    result = (1 / val).toFixed(fix);
+                }
+            }
+            return result;
         }
     },
 }
 </script>
 
 <style scoped>
-.list{
+.list {
     max-height: 60vh;
     overflow-y: auto;
     border-radius: 20px;
 }
-.list__item{
+
+.list__item {
     border-bottom: 1px solid rgb(61, 61, 61);
 }
+
 * {
-  scrollbar-width: thin;
+    scrollbar-width: thin;
 }
+
 *::-webkit-scrollbar {
-  width: 12px;
+    width: 12px;
 }
+
 *::-webkit-scrollbar-thumb {
-  border-radius: 20px;
-  border: 3px solid;
+    border-radius: 20px;
+    border: 3px solid;
 }
 </style>
